2bbd155ab69bf45af376c1fbad2e7003db9ad63d
488-4-Natdanai <Natdanai.sir@kkumail.com>
2025-12-25 13:08:30 +0700

    commit

--- Lab4/Lab4-2/P1.py
+++ Lab4/Lab4-2/P1.py
@@ -6,13 +6,16 @@ sarah_saving = BankAccount("Sarah")
 
 john.deposit(3000)
 john.withdraw(3500)
+
 tim.pay_loan(500_000)
 tim.get_loan(3_000_000)
-sarah_saving.deposit(50_000_000)
 
+sarah_saving.deposit(50_000_000)
 sarah_loan = BankAccount("Sarah", "loan", -100_000_000)
 accounts = [john, tim, sarah_saving, sarah_loan]
 
 for acc in accounts:
     acc.print_customer()
 clac_interest(1000, 5, 100)
+john.change_branch_name()
+print(f"{john.name}'s branch is now {john.branch_name}")
