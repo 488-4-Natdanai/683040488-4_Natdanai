7ddefcdb2cbb56ff28a8bc76a3222ba700bdb788
488-4-Natdanai <natdanai.sir@kkumail.com>
2026-02-12 01:23:28 +0700

    a

--- Lab5/Lab5-1/P3.py
+++ Lab5/Lab5-1/P3.py
@@ -3,7 +3,7 @@ from PySide6.QtWidgets import (
     QApplication, QWidget, QLabel, QLineEdit,
     QVBoxLayout, QHBoxLayout, QPushButton,
     QComboBox, QGroupBox, QGridLayout,
-    QMainWindow, QTableWidget, QTableWidgetItem
+    QMainWindow, QTableWidget, QTableWidgetItem, QFormLayout
 )
 from PySide6.QtCore import Qt
 
@@ -32,41 +32,38 @@ class BMICalculator(QWidget):
         # -------------------------
         # Form Section
         # -------------------------
-        
-        age_layout = QHBoxLayout()
-        age_layout.setAlignment(Qt.AlignCenter)
-        # Age group
-        age_layout.addWidget(QLabel("Calculate BMI for"))
+        form_layout = QFormLayout()
+        form_layout.setLabelAlignment(Qt.AlignRight)
+        form_layout.setFormAlignment(Qt.AlignTop)
 
+        # Age Group
         self.age_combo = QComboBox()
         self.age_combo.addItems([
             "Adult Age 20+",
             "Child (2 - 19)"
         ])
-        age_layout.addWidget(self.age_combo)
+        form_layout.addRow("BMI age group:", self.age_combo)
 
-        form_layout = QGridLayout()
-        form_layout.setContentsMargins(35, 0, 35, 10)
         # Weight
-        form_layout.addWidget(QLabel("Weight:"), 1, 0)
-
+        weight_layout = QHBoxLayout()
         self.weight_input = QLineEdit()
         self.weight_input.setFixedWidth(80)
-        form_layout.addWidget(self.weight_input, 1, 1)
 
         self.weight_unit = QComboBox()
         self.weight_unit.addItems([
             "kilograms",
             "pounds"
         ])
-        form_layout.addWidget(self.weight_unit, 1, 2)
 
-        # Height (feet + inches OR meters)
-        form_layout.addWidget(QLabel("Height:"), 2, 0)
+        weight_layout.addWidget(self.weight_input)
+        weight_layout.addWidget(self.weight_unit)
 
+        form_layout.addRow("Weight:", weight_layout)
+
+        # Height
+        height_layout = QHBoxLayout()
         self.height_input = QLineEdit()
         self.height_input.setFixedWidth(80)
-        form_layout.addWidget(self.height_input, 2, 1)
 
         self.height_unit = QComboBox()
         self.height_unit.addItems([
@@ -74,17 +71,14 @@ class BMICalculator(QWidget):
             "centimeters",
             "feet"
         ])
-        form_layout.addWidget(self.height_unit, 2, 2)
 
-        # Inches input (used if feet selected)
-        self.inches_input = QLineEdit()
-        self.inches_input.setFixedWidth(80)
-        form_layout.addWidget(self.inches_input, 3, 1)
+        height_layout.addWidget(self.height_input)
+        height_layout.addWidget(self.height_unit)
 
-        inches_label = QLabel("inches")
-        form_layout.addWidget(inches_label, 3, 2)
-        main_layout.addLayout(age_layout)
+        form_layout.addRow("Height:", height_layout)
+
         main_layout.addLayout(form_layout)
+
 
         # -------------------------
         # Buttons
@@ -92,40 +86,45 @@ class BMICalculator(QWidget):
         button_layout = QHBoxLayout()
 
         self.clear_button = QPushButton("Clear")
+        self.clear_button.setFixedSize(80,35)
         self.calculate_button = QPushButton("Calculate")
+        self.calculate_button.setFixedHeight(35)
 
         button_layout.addWidget(self.clear_button)
-        button_layout.addStretch()
         button_layout.addWidget(self.calculate_button)
 
         main_layout.addLayout(button_layout)
 
         # -------------------------
-        # Answer Section
+        # result Section
         # -------------------------
-        answer_group = QGroupBox("Answer:")
-        answer_layout = QVBoxLayout()
+        result_group = QGroupBox()
+        result_layout = QVBoxLayout()
 
-        self.result_label = QLabel("BMI = ")
-        self.result_label.setAlignment(Qt.AlignCenter)
-        self.result_label.setStyleSheet("font-weight: bold;")
+        self.your_label = QLabel("Your BMI: ")
+        self.your_label.setAlignment(Qt.AlignCenter)
+        self.your_label.setStyleSheet("font-weight: bold;")
 
-        answer_layout.addWidget(self.result_label)
+        result_layout.addWidget(self.your_label)
+
+        result_label = QLabel("0.00")
+        result_label.setAlignment(Qt.AlignCenter)
+        result_label.setStyleSheet("font-size: 30px; font-weight: bold;")
+        result_layout.addWidget(result_label)
+        result_layout.setAlignment(result_label, Qt.AlignTop | Qt.AlignHCenter)
+        result_layout.addSpacing(20)
 
         # -------------------------
         # BMI Table (Adult)
         # -------------------------
-        table_label = QLabel("Adult BMI")
-        table_label.setAlignment(Qt.AlignCenter)
-        table_label.setStyleSheet("font-weight: bold;")
-
-        answer_layout.addWidget(table_label)
-
         self.bmi_table = QTableWidget(4, 2)
         self.bmi_table.setHorizontalHeaderLabels(["BMI", "Status"])
         self.bmi_table.verticalHeader().setVisible(False)
         self.bmi_table.setEditTriggers(QTableWidget.NoEditTriggers)
-
+        self.bmi_table.setFixedHeight(150)
+        self.bmi_table.setColumnWidth(0, 120)
+        self.bmi_table.setColumnWidth(1, 120)
+        self.bmi_table.horizontalHeader().setStretchLastSection(True)
         data = [
             ("≤ 18.4", "Underweight"),
             ("18.5 - 24.9", "Normal"),
@@ -137,10 +136,10 @@ class BMICalculator(QWidget):
             self.bmi_table.setItem(row, 0, QTableWidgetItem(bmi))
             self.bmi_table.setItem(row, 1, QTableWidgetItem(status))
 
-        answer_layout.addWidget(self.bmi_table)
-        answer_group.setLayout(answer_layout)
+        result_layout.addWidget(self.bmi_table)
+        result_group.setLayout(result_layout)
 
-        main_layout.addWidget(answer_group)
+        main_layout.addWidget(result_group)
 
         self.setLayout(main_layout)
 
@@ -149,7 +148,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = QMainWindow()
     window.setWindowTitle("BMI Calculator")
-    window.setFixedSize(300, 470)
+    window.setFixedSize(300, 480)
     window.setCentralWidget(BMICalculator())
     window.show()
     sys.exit(app.exec())
