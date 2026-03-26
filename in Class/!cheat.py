d18fd5df1ed4517cd04ae0b88d0797834f0afb28
488-4-Natdanai <natdanai.sir@kkumail.com>
2026-03-26 12:53:26 +0700

    a

--- in Class/!cheat.py
+++ in Class/!cheat.py
@@ -3,7 +3,7 @@ from PySide6.QtWidgets import (QApplication, QWidget, QMainWindow, QVBoxLayout, 
                                QLabel,QPushButton, QLineEdit, QCheckBox, QComboBox, QSpinBox, QMessageBox, QTextEdit)
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap, QFont
-
+current_dir = os.path.dirname(os.path.abspath(__file__))
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -11,29 +11,42 @@ class MainWindow(QMainWindow):
         self.setFixedSize(400, 500)
         self.setMinimumSize(300, 400)
         self.setGeometry(100, 100, 400, 500)
-        QVBoxLayout()
-        QHBoxLayout()
-        QGridLayout()
-        QFormLayout()
-        QPushButton
-        QLineEdit
-        QCheckBox
-        QComboBox
-        QSpinBox
-        QMessageBox
-        QTextEdit
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         layout = QVBoxLayout(central_widget)
 
-        text_label = QLabel("Hello World")
-        text_label.setFont(QFont("Arial", 14, QFont.Bold))
-        text_label.setAlignment(Qt.AlignCenter)
-        layout.addWidget(text_label)
+        text = QLabel("Hello World")
+        text.setFont(QFont("Arial", 14, QFont.Bold))
+        text.setAlignment(Qt.AlignCenter)
+        text.setText("World Hello ")
+
+        bt = QPushButton("text")
+        bt.clicked.connect(lambda: text.setText("Wello Horld"))
+        bt.setCheckable(True)
+
+        le = QLineEdit()
+        le.setPlaceholderText("Enter text here")
+        le.setText("Initial text")
+        le.setEchoMode(QLineEdit.Password) # ซ่อนข้อความ
+        # อัพเดตข้อความใน QLabel ตามที่พิมพ์ใน QLineEdit
+        le.textChanged.connect(lambda: text.setText(le.text())) 
+    
+        cb = QCheckBox("text")
+        # เช็คสถานะของ CheckBox และอัพเดตข้อความใน QLabel ตามสถานะ
+        cb.stateChanged.connect(lambda: text.setText("Checked") 
+                                if cb.isChecked() else text.setText("Not checked"))
+        comb = QComboBox
+        comb.addItems([1,2,3])
+        comb.currentText()
+
+        sb = QSpinBox
+        sb.setRange(0, 60) #min max
+        sb.value() #ดึงค่าปัจจุบัน
+        
+        layout.addWidget(text)
         
         image_label = QLabel()
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        image_path = os.path.join(current_dir, "face.png")
+        image_path = os.path.join(current_dir, "img.png")
         pixmap = QPixmap(image_path)
         image_label.setPixmap(pixmap.scaled(200, 200,
             Qt.KeepAspectRatio,  # maintain aspect ratio
